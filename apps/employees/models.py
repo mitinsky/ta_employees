6fc43e8797e790023e9ffa554b8b33bcc08c265b
Vasily Mitinsky <mitinsky@metasharks.ru>
2023-05-30 15:18:03 +0300

    models in employees app + migration, pillow in requirements

--- apps/employees/models.py
+++ apps/employees/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    name = models.CharField(blank=False, db_index=True, max_length=200)
+    photo = models.ImageField()
+    position = models.CharField(blank=False, max_length=200)
+    salary = models.DecimalField(decimal_places=2, max_digits=11)
+    # TODO лучше хранить дату рождения чем возраст
+    birthday = models.DateField()
+    department = models.ForeignKey('Department', on_delete=models.PROTECT)
+
+
+class Department(models.Model):
+    title = models.CharField(blank=False, max_length=200)
+    # TODO связь с директором лучше сделать через django_guardian, если будет время
+    # director = models.OneToOneField(Employee, on_delete=models.PROTECT, related_name='managed_department')
