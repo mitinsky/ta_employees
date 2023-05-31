eddce484a4b31d4f07f197443855656434348d04
Vasily Mitinsky <mitinsky@metasharks.ru>
2023-05-31 09:26:05 +0300

    django-filter in requirements + settings drf DEFAULT_FILTER_BACKEND

--- project/settings.py
+++ project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'django_filters',
 
     'apps.employees',
 ]
@@ -133,4 +134,5 @@ MEDIA_URL = '/media/'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
