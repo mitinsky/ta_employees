21640a0b8a4faef7a4a5d4cbdf61af18f6d72adc
Vasily Mitinsky <mitinsky@metasharks.ru>
2023-06-01 12:17:38 +0300

    tests for create/destroy employee api

--- tests/test_employees/test_api.py
+++ tests/test_employees/test_api.py
@@ -1,8 +1,13 @@
 import random
 import pytest
+from io import BytesIO
+from PIL import Image
 
 from django.urls import reverse
+from django.core.files.base import File
 from rest_framework import status
+
+from apps.employees.models import Employee
 
 
 pytestmark = pytest.mark.django_db
@@ -11,6 +16,16 @@ pytestmark = pytest.mark.django_db
 @pytest.fixture
 def client(api_client):
     return api_client()
+
+@pytest.fixture
+def userpic():
+    # TODO use tmpdir
+    file_obj = BytesIO()
+    image = Image.new('RGBA', size=(50, 50), color=(255, 0, 0))
+    image.save(file_obj, 'png')
+    file_obj.seek(0)
+
+    return File(file_obj, name='userpic.png')
 
 @pytest.fixture
 def department_first(department_factory):
@@ -36,9 +51,21 @@ def batch_employees_second_dep(employee_factory, department_second):
 def target_employee(batch_employees_20):
     return random.choice(batch_employees_20)
 
+@pytest.fixture
+def new_employee_data(department_first, userpic):
+    return {
+        'name': 'Test test test',
+        'photo': userpic,
+        'position': 'position',
+        'salary': 99999.99,
+        'birthday': '1990-01-01',
+        'department': department_first.id,
+    }
+
 
 class TestEmployeesEndpoints:
     url_list = 'employee-list'
+    url_detail = 'employee-detail'
 
     def test_list(self, client, batch_employees_20):
         response = client.get(reverse(self.url_list))
@@ -48,6 +75,7 @@ class TestEmployeesEndpoints:
         # test pagination
         assert len(response.data['results']) == 10
 
+    # TODO parametrize
     def test_list_filter_name(self, client, batch_employees_20, target_employee):
         search_name = target_employee.name.split()[0]  # last_name
         response = client.get(reverse(self.url_list), {'name': search_name})
@@ -68,3 +96,16 @@ class TestEmployeesEndpoints:
 
         assert response.status_code == status.HTTP_200_OK
         assert response.data['count'] == 20
+
+    def test_create_employee(self, client, new_employee_data):
+        response = client.post(reverse(self.url_list), data=new_employee_data)
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['name'] == new_employee_data['name']
+
+    def test_destroy_employee(self, client, batch_employees_20, target_employee):
+        expected_employees_count = len(batch_employees_20) - 1
+        response = client.delete(reverse(self.url_detail, args=[target_employee.id]))
+
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+        assert Employee.objects.count() == expected_employees_count
